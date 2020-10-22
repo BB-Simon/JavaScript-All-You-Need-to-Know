107111809ea2a654738bf5c022f8d936cd1afba4
BB-Simon <simon.chowdery@gmail.com>
2020-10-21 23:51:52 -0800

    added

--- JS-OOP.js
+++ JS-OOP.js
@@ -1,1 +1,14 @@
+class Book {
+  name = "Js Eco";
+  author = "Simon Chowdery";
+  publishedAt = "20-12-2018";
 
+  getBookInfo() {
+    console.log(
+      `Book name: ${this.name} and it's Writen by ${this.author} and published at ${this.publishedAt}`
+    );
+  }
+}
+
+let book1 = new Book();
+book1.getBookInfo();
