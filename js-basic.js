f3e8f776f0122c7de97e0f5205ce2a4d3c556a7d
BB-Simon <simon.chowdery@gmail.com>
2021-07-10 02:19:01 -0800

    how to update an object property

--- js-basic.js
+++ js-basic.js
@@ -534,3 +534,47 @@ var numbers = [1, 2, 3, 4, 5, 6];
 // @param2? fromIndex — The array index at which to begin the search. If fromIndex is omitted, the search starts at index 0.
 // arr.indexOf("jimon");
 // arr.indexOf("jimon", 1);
+
+// *******************************************************
+// ************* @Object In JS ***************
+// *******************************************************
+// Setup
+var recordCollection = {
+  2548: {
+    albumTitle: "Slippery When Wet",
+    artist: "Bon Jovi",
+    tracks: ["Let It Rock", "You Give Love a Bad Name"],
+  },
+  2468: {
+    albumTitle: "1999",
+    artist: "Prince",
+    tracks: ["1999", "Little Red Corvette"],
+  },
+  1245: {
+    artist: "Robert Palmer",
+    tracks: [],
+  },
+  5439: {
+    albumTitle: "ABBA Gold",
+  },
+};
+
+// Only change code below this line
+function updateRecords(records, id, prop, value) {
+  if (prop !== "tracks" && value !== "") {
+    records[id][prop] = value;
+  } else if (prop === "tracks" && !records[id].hasOwnProperty(prop)) {
+    records[id][prop] = [value];
+  } else if (
+    prop === "tracks" &&
+    value !== "" &&
+    records[id].hasOwnProperty(prop)
+  ) {
+    records[id][prop].push(value);
+  } else if (value === "") {
+    delete records[id].prop;
+  }
+  console.log(records);
+}
+
+updateRecords(recordCollection, 2548, "artist", "");
