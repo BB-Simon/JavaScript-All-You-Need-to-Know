850576d2d25dbf7ce08f28c3de7995226d3557bf
BB-Simon <simon.chowdery@gmail.com>
2021-07-24 01:34:49 -0800

    update

--- js-functional.js
+++ js-functional.js
@@ -419,19 +419,19 @@
 // ***********************************************************
 
 // *****************first class function **************
-// Functions that can be assigned to a variable, passed into 
-// another function, or returned from another function just 
-// like any other normal value, are called first class functions. 
+// Functions that can be assigned to a variable, passed into
+// another function, or returned from another function just
+// like any other normal value, are called first class functions.
 // In JavaScript, all functions are first class functions.
 
 // ***************** higher order function **************
-// The functions that take a function as an argument, or 
-// return a function as a return value are called higher order 
+// The functions that take a function as an argument, or
+// return a function as a return value are called higher order
 // functions.
 
 // ***************** lambda function **************
-// When functions are passed in to or returned from another 
-// function, then those functions which were passed in or 
+// When functions are passed in to or returned from another
+// function, then those functions which were passed in or
 // returned can be called a lambda
 
 // var watchList = [
@@ -567,3 +567,9 @@
 // // Only change code above this line
 
 // console.log(JSON.stringify(ratings));
+
+function sayHello(name) {
+  console.log(`Hello ${name}`);
+}
+
+sayHello("Simon")
