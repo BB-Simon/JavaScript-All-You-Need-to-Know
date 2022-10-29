11b0e732e5f2a391b04623c98705ee27c7be0d9f
BB-Simon <simon.chowdery@gmail.com>
2022-10-29 19:15:18 +0200

    counting sort

--- problem-solving/index.js
+++ problem-solving/index.js
@@ -729,3 +729,34 @@ function quickSort(arr) {
 }
 
 quickSort([4, 5, 3, 7, 2])
+
+/**
+ * Complete the 'countingSort' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function countingSort(arr) {
+    // Write your code here
+    const zeros = Array.from({length:100},()=> 0)
+    const range = [...Array(100).keys()]
+   
+    for(let i = 0; i < range.length; i++) {
+        for(let j = 0; j < arr.length; j++) {
+            if(range[i] === arr[j]){
+                zeros[i]+=1
+            }  
+        }
+        
+    }
+    return zeros
+
+}
+const f = [63, 25, 73, 1, 98, 73, 56, 84, 86, 57, 16, 83, 8, 25, 81, 56, 9, 53, 98, 67, 99, 12, 83, 89, 80, 91, 39, 86, 
+    76, 85, 74, 39, 25, 90, 59, 10, 94, 32, 44, 3, 89, 30, 27, 79, 46, 96, 
+    27, 32, 18, 21, 92, 69, 81, 40, 40, 34 ,68, 78, 24, 87, 42, 69, 23, 41, 
+    78, 22, 6 ,90 ,99 ,89 ,50 ,30 ,20 , 1, 43, 3, 70, 95, 33, 46, 44, 9, 69, 48 ,
+    33, 60, 65, 16, 82, 67, 61, 32, 21, 79 ,75, 75, 13, 87, 70, 33]
+
+countingSort(f)
