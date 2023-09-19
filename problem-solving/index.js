2b7dfe72a7d9aa169ff5d3423a2132b1defccc4f
BB-Simon <simon.chowdery@gmail.com>
2023-09-19 17:57:54 +0300

    Roman to integer

--- problem-solving/index.js
+++ problem-solving/index.js
@@ -1348,3 +1348,70 @@ var longestCommonPrefix = function (strs) {
 };
 
 // longestCommonPrefix(["aaa", "aa", "aaa"])
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function (s) {
+    // const roman = {
+    //     'I': 1,
+    //     'V': 5,
+    //     'X': 10,
+    //     'L': 50,
+    //     'C': 100,
+    //     'D': 500,
+    //     'M': 1000,
+    // }
+    
+    // let res = 0;
+    // let i = s.length -1;
+
+    // while(i >= 0){
+    //     console.log(s[i]);
+    //     if((s[i] === 'V' || s[i] === 'X') && s[i - 1] === 'I'){
+    //         res+= roman[s[i]] - roman[s[i - 1]]
+    //         i-=2;
+    //     } else if((s[i] === 'L' || s[i] === 'C') && s[i - 1] === 'X') {
+    //         res += roman[s[i]] - roman[s[i - 1]]
+    //         i -= 2;
+    //     } else if ((s[i] === 'D' || s[i] === 'M') && s[i - 1] === 'C'){
+    //         res+= (roman[s[i]] - roman[s[i - 1]])
+    //         i -= 2;
+    //     } else {
+    //         res += roman[s[i]];
+    //         i--;
+    //     }
+    // }
+
+    // return res;
+
+    const roman = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000,
+    }
+
+    let res = 0;
+    let i = 0;
+
+    while (i < s.length) {
+        const curr = roman[s[i]];
+        const next = roman[s[i + 1]];
+        if (curr < next) {
+            res += next - curr;
+            i += 2
+        } else {
+            res += curr
+            i++
+        }
+    }
+
+    return res;
+};
+
+// romanToInt("MCMXCIV")
