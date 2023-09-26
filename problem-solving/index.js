2f67ff5b459f0691a40b78049cd93739d5aec995
BB-Simon <simon.chowdery@gmail.com>
2023-09-26 22:20:44 +0300

    Restore string

--- problem-solving/index.js
+++ problem-solving/index.js
@@ -1568,3 +1568,19 @@ var balancedStringSplit = function (s) {
     }
     return split
 };
+
+/**
+ * @param {string} s
+ * @param {number[]} indices
+ * @return {string}
+ */
+var restoreString = function (s, indices) {
+    let obj = {};
+    let res = '';
+    for (let i = 0; i < s.length; i++) {
+        obj = { ...obj, [indices[i]]: s[i] }
+    }
+    Object.keys(obj).forEach((k) => res += obj[k]);
+
+    return res;
+};
