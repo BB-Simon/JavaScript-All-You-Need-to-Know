5b686fc45055b3fc7ab6e865da6546503fda8a61
BB-Simon <simon.chowdery@gmail.com>
2023-10-03 12:31:52 +0300

    Add reverse string method

--- ds&algo/array.js
+++ ds&algo/array.js
@@ -13,4 +13,24 @@ function reverse(arr){
   return arr;
 }
 
-// reverse([1, 2, 3, 4, 5, 6])
+
+// let arr = [1, 2, 3, 4, 5, 6]
+// reverse(arr)
+
+function reverseStr(str) {
+  str = str.split('')
+  let start = 0;
+  let end = str.length - 1;
+
+  while (start < end) {
+    let temp = str[start];
+    str[start] = str[end];
+    str[end] = temp;
+    start += 1;
+    end -= 1;
+  }
+  return str.join('');
+}
+
+let str = 'simon'
+reverseStr(str)
