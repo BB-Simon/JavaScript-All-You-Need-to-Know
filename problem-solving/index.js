ac07fc04022dd4069e0d00595c361be505f7dc91
BB-Simon <simon.chowdery@gmail.com>
2024-06-02 16:32:57 +0300

    Solve beautiful triplets

--- problem-solving/index.js
+++ problem-solving/index.js
@@ -1752,6 +1752,31 @@ function kaprekarNumbers(p, q) {
     }
 }
 
-kaprekarNumbers(1, 10000);
+// kaprekarNumbers(1, 10000);
+
+
+/*
+ * Complete the 'beautifulTriplets' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER d
+ *  2. INTEGER_ARRAY arr
+ */
+
+function beautifulTriplets(d, arr) {
+    // Write your code here
+
+    const mySet = new Set(arr);
+    let count = 0;
+    for(let i = 0; i < arr.length - 2; i++) {
+        if (mySet.has(arr[i] + d) && mySet.has(arr[i] + (2 * d))){
+            count +=1;
+        }
+    }
+    return count;
+}
+
+// console.log(beautifulTriplets(3, [1, 2, 4, 5, 7, 8, 10]))
 
 
